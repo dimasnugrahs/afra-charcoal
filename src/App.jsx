5921dc24fac73dda7e0a9158d40460a91f05cd70
dimasnugrahs <79310230+dimasnugrahs@users.noreply.github.com>
2025-11-05 23:55:00 +0800

    update scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
-import ScrollToTop from "./components/layout/ScrollToTop";
 import ContactPage from "./pages/ContactPage";
 import ProductPage from "./pages/ProductPage";
+import ScrollToTop from "./components/layout/ScrollToTop";
 
 function App() {
   return (
